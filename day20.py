2b2fd4df7e56c9eb64ae4a8fbd824dc6ebb9d1df
madongyoun9594 <madongyoun9594@gmail.com>
2024-02-08 13:49:57 +0900

    v0.7 guess number

--- day20.py
+++ day20.py
@@ -1,8 +1,21 @@
-import mymath
+import random
 
-if __name__ == "__main__":
-    n = int(input("Input n : "))
-    r = int(input("Input r : "))
-    print(f"{n}C{r} = {mymath.nCr(n, r)}")
-    # f = int(input())
-    # print(mymath.factorial(f))
+answer = random.randint(1,100)
+chance = 7
+
+while chance != 0:
+    num = int(input("1~100사이의 숫자를 입력하세요 : "))
+    if(num == answer):
+        print(f" 정답입니다.")
+        break
+    elif(num > answer):
+        chance = chance - 1
+        print("답이 더 작습니다.")
+
+    else:
+        chance = chance - 1
+        print("답이 더 큽니다.")
+
+else:
+    print("ㅂㅅ")
+
